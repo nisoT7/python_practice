a2332640b3ee5970c70e0c55d04fe59ddecb1cb8
nisot7 <t.niso0710@gmail.com>
2016-02-26 10:13:54 +0900

    ""

--- git_upload.sh
+++ git_upload.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 git add -A
+
 if [ $# != 0 ]; then
     git commit -m "$1"
 else
-
-   datetime = date '+%Y_%m_%d_%H_%M_%S'
-   git commit -m "datetime"
+    git commit -m \"${date}\"
 fi
 
 git push -u origin master
